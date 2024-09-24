70f4eda861beaffff83b7e7b6283a49d3adebf40
Alberto Bonacina <bonacina.alberto@gmail.com>
2024-09-25 00:02:56 +0200

    add: Add riverpod for the state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_deck/flutter_deck.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:linuxday_2024_presentation/provider/service_locator.dart';
+
+import 'slides/0_first_slide.dart';
+import 'slides/split_slide.dart';
 
 void main() {
-  runApp(const MainApp());
+  setupLocator();
+
+  runApp(const ProviderScope(child: LinuxDayPresentation()));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class LinuxDayPresentation extends StatelessWidget {
+  const LinuxDayPresentation({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return FlutterDeckApp(
+      speakerInfo: const FlutterDeckSpeakerInfo(
+        name: 'Alberto Bonacina',
+        description:
+            '🚀 Passionate Software Engineer | 💙 Flutter Enthusiast | 🐧 Advocate for Free Software and Linux',
+        socialHandle: '@polilluminato',
+        imagePath: 'assets/images/me.png',
       ),
+      configuration: const FlutterDeckConfiguration(
+        background: FlutterDeckBackgroundConfiguration(
+          light: FlutterDeckBackground.solid(Color(0xFFB5FFFC)),
+          dark: FlutterDeckBackground.solid(Color(0xFF16222A)),
+        ),
+        controls: FlutterDeckControlsConfiguration(
+          presenterToolbarVisible: true,
+          shortcuts: FlutterDeckShortcutsConfiguration(
+            enabled: true,
+            nextSlide: SingleActivator(LogicalKeyboardKey.arrowRight),
+            previousSlide: SingleActivator(LogicalKeyboardKey.arrowLeft),
+            toggleMarker: SingleActivator(
+              LogicalKeyboardKey.keyM,
+              control: true,
+              meta: true,
+            ),
+            toggleNavigationDrawer: SingleActivator(
+              LogicalKeyboardKey.period,
+              control: true,
+              meta: true,
+            ),
+          ),
+        ),
+        footer: FlutterDeckFooterConfiguration(
+          showSlideNumbers: false,
+          showSocialHandle: true,
+        ),
+        header: FlutterDeckHeaderConfiguration(
+          showHeader: false,
+        ),
+        showProgress: true,
+        transition: FlutterDeckTransition.fade(),
+      ),
+      slides: const [
+        FirstSlide(),
+        SplitSlide(),
+      ],
     );
   }
 }
