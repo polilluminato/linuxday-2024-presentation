1f9a0974e18b22876c1b1e3cf4bda509d286ac42
Alberto Bonacina <bonacina.alberto@gmail.com>
2024-10-26 06:58:59 +0200

    chore: Upgrade dependencies

--- lib/provider/database_provider.g.dart
+++ lib/provider/database_provider.g.dart
@@ -19,6 +19,8 @@ final databaseProvider = AutoDisposeProvider<AppDatabase>.internal(
   allTransitiveDependencies: null,
 );
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 typedef DatabaseRef = AutoDisposeProviderRef<AppDatabase>;
 String _$allAddressBookHash() => r'99fc875270128b2830313e8df9cea93f07480b74';
 
@@ -35,9 +37,11 @@ final allAddressBookProvider =
   allTransitiveDependencies: null,
 );
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 typedef AllAddressBookRef = AutoDisposeFutureProviderRef<List<AddressBookData>>;
 String _$addRandomAddressBookHash() =>
-    r'1f17e5817eb7c5509e42583a3591c71cc74b76c1';
+    r'a83787c8153d1f78f129649324d158890f154bae';
 
 /// See also [addRandomAddressBook].
 @ProviderFor(addRandomAddressBook)
@@ -51,9 +55,11 @@ final addRandomAddressBookProvider = AutoDisposeFutureProvider<void>.internal(
   allTransitiveDependencies: null,
 );
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 typedef AddRandomAddressBookRef = AutoDisposeFutureProviderRef<void>;
 String _$deleteOldestAddressBookHash() =>
-    r'cabaaca01e272684e6f5258ceec7ec30d1a3d6c5';
+    r'aa2f1dd85e2063a6e594f0f206d7929fc3f1e7ce';
 
 /// See also [deleteOldestAddressBook].
 @ProviderFor(deleteOldestAddressBook)
@@ -68,6 +74,8 @@ final deleteOldestAddressBookProvider =
   allTransitiveDependencies: null,
 );
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 typedef DeleteOldestAddressBookRef = AutoDisposeFutureProviderRef<void>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/provider/pubclient_provider.g.dart
+++ lib/provider/pubclient_provider.g.dart
@@ -142,6 +142,8 @@ class AllInfoPackageProvider extends AutoDisposeFutureProvider<AllInfoPackage> {
   }
 }
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 mixin AllInfoPackageRef on AutoDisposeFutureProviderRef<AllInfoPackage> {
   /// The parameter `packageName` of this provider.
   String get packageName;
@@ -156,4 +158,4 @@ class _AllInfoPackageProviderElement
   String get packageName => (origin as AllInfoPackageProvider).packageName;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
